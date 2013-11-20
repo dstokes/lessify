3beed745a6b2f95e4e49b02476ced7794f19fc22
Drew Stokes <drew@drewstokes.com>
2013-11-20 11:44:02 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+lessify
+=======
+Middleware and Brologrotate-streamwserify transform for less files.
+
+[![NPM](https://nodei.co/npm/lessify.png?downloads=true)](https://nodei.co/npm/lessify/)
+
+usage
+=====
+some.less
+``` less
+  .nav { width: (1 + 1); }
+```
+
+entry.js
+```
+require('some.less');
+
+// => we haz css in our bundle!
+```
+
+install
+=======
+
+With [npm](http://npmjs.org) do:
+
+```
+npm install lessify
+```
---
+++ index.js
@@ -0,0 +1,24 @@
+var less = require("less")
+  , through = require('through');
+
+module.exports = function(file) {
+  var input = '';
+  if (/\.less$/i.test(file) === false) {
+    return through(); 
+  } 
+
+  function write(data) { input += data; }
+  function end() {
+    var self = this;
+    less.render(input, function(err, css) {
+      if (err) throw err;
+      css = css.replace(/\"/g, "\\\"").replace(/\n/g, "\\\n");
+      var body = "var css = '"+ css +"';"+ 
+                 "(require('lessify'))(css); module.exports = css;";
+      self.queue(body);
+      self.queue(null);
+    });
+  }
+
+  return through(write, end);
+}
